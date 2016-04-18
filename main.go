38cbdf2c6a94c84c4fc44803023b7ca088d90ca2
Adrián López Gómez <adrian.lopez.gomez@tecsisa.com>
2016-04-18 16:06:52 +0200

    binding info

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	core := authorizr.NewCore()
+	fmt.Printf("Server running - binding :8000")
 	err := http.ListenAndServe(":8000", internalhttp.Handler(core))
 	fmt.Errorf(err.Error())
 }
