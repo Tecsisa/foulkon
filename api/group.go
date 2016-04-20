7eae5cccebb931456037d681e48a0afd08dfde09
Adrián López Gómez <adrian.lopez.gomez@tecsisa.com>
2016-04-20 18:16:52 +0200

    fix formatting

--- api/group.go
+++ api/group.go
@@ -3,19 +3,17 @@ package api
 import "time"
 
 type GroupAPI struct {
-
 }
 
-func (u *GroupAPI) GetGroups(path string) (string)  {
+func (u *GroupAPI) GetGroups(path string) string {
 	return path
 }
 
 type Group struct {
-	Id 	uint64 		`json:"ID, omitempty"`
-	Name 	string 		`json:"Name, omitempty"`
-	Path 	string 		`json:"Path, omitempty"`
-	Date 	time.Time 	`json:"Date, omitempty"`
-	Urn 	string 		`json:"Urn, omitempty"`
-	Org 	string 		`json:"Org, omitempty"`
+	Id   uint64    `json:"ID, omitempty"`
+	Name string    `json:"Name, omitempty"`
+	Path string    `json:"Path, omitempty"`
+	Date time.Time `json:"Date, omitempty"`
+	Urn  string    `json:"Urn, omitempty"`
+	Org  string    `json:"Org, omitempty"`
 }
-
