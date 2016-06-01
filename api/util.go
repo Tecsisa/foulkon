cfcf82c0f9f87ceed0e37c107494bb0b9d034a96
Adrián López Gómez <adrian.lopez.gomez@tecsisa.com>
2016-06-01 11:25:22 +0200

    fix createUrn for users

--- api/util.go
+++ api/util.go
@@ -55,7 +55,7 @@ const (
 func CreateUrn(org string, resource string, path string, name string) string {
 	switch resource {
 	case RESOURCE_USER:
-		return fmt.Sprintf("urn:iws:iam:user%v%v", path, name)
+		return fmt.Sprintf("urn:iws:iam::user%v%v", path, name)
 	default:
 		return fmt.Sprintf("urn:iws:iam:%v:%v%v%v", org, resource, path, name)
 	}
@@ -64,7 +64,7 @@ func CreateUrn(org string, resource string, path string, name string) string {
 func GetUrnPrefix(org string, resource string, path string) string {
 	switch resource {
 	case RESOURCE_USER:
-		return fmt.Sprintf("urn:iws:iam:user%v*", path)
+		return fmt.Sprintf("urn:iws:iam::user%v*", path)
 	default:
 		return fmt.Sprintf("urn:iws:iam:%v:%v%v*", org, resource, path)
 	}
