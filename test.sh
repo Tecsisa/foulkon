3a92b0d6bffae1787e31f23268fa2a2f35207dcc
Rubén Soleto Buenvarón <ruben.soleto.buenvaron@tecsisa.com>
2016-06-24 14:27:46 +0200

    test.sh permissions changed

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-go list ./... | grep -v '/vendor/' | sudo -E PATH=$TEMPDIR:$PATH xargs -n1 go test ${GOTEST_FLAGS:--cover -timeout=900s}
+go list ./... | grep -v '/vendor/' | PATH=$TEMPDIR:$PATH xargs -n1 go test ${GOTEST_FLAGS:--cover -timeout=900s}
