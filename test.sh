afa3e3f0c550072b52624f976692cd88cfb67eec
Rubén Soleto Buenvarón <ruben.soleto.buenvaron@tecsisa.com>
2016-07-11 14:05:16 +0200

    Script test messages refactor

--- test.sh
+++ test.sh
@@ -1,8 +1,16 @@
-echo -e '- Starting authorizr unit test'
+echo -e '\n################################\n# Starting authorizr unit test #\n################################\n'
+
 go list ./... | grep -v '/vendor/' | grep -v '/database/' | PATH=$TEMPDIR:$PATH xargs -n1 go test ${GOTEST_FLAGS:--cover -timeout=900s}
-echo -e '- Starting connectors test'
-echo -e '-- Starting test for PostgreSQL connector'
-echo $(echo -e '--- Starting PostgreSQL container postgrestest with id ') $(docker run --name postgrestest -p 54320:5432 -e POSTGRES_PASSWORD=password -d postgres)
-echo -e '--- Starting authorizr test for PostgreSQL connector'
+
+echo -e '\n############################\n# Starting connectors test #\n############################\n'
+
+# Postgres
+echo -e '########################\n# PostgreSQL connector #\n########################'
+echo $(echo -e 'Starting PostgreSQL container postgrestest with id ') \
+$(docker run --name postgrestest -p 54320:5432 -e POSTGRES_PASSWORD=password -d postgres) \
+$(echo -e '\n')
+echo -e 'Starting authorizr test for PostgreSQL connector \n'
 go test ./database/postgresql ${GOTEST_FLAGS:--cover -timeout=900s}
-echo $(echo -e '--- Removing PostgreSQL container ') $(docker rm -f postgrestest)
+echo -e '\nRemoving PostgreSQL container' $(docker rm -f postgrestest)
+
+echo -e '\n############################\n# Connectors test finished #\n############################\n'
