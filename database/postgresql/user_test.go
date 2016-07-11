0beb1a94e59de4df8f5c0ab1d78d9b79fa302605
Rubén Soleto Buenvarón <ruben.soleto.buenvaron@tecsisa.com>
2016-07-11 15:24:58 +0200

    Test GetUsersFiltered for postgres connector added

--- database/postgresql/user_test.go
+++ database/postgresql/user_test.go
@@ -91,7 +91,7 @@ func TestPostgresRepo_AddUser(t *testing.T) {
 			}
 			// Check database
 			userNumber, err := getUsersCountFiltered(test.expectedResponse.ID, test.expectedResponse.ExternalID, test.expectedResponse.Path,
-				test.expectedResponse.CreateAt.UnixNano(), test.expectedResponse.Urn)
+				test.expectedResponse.CreateAt.UnixNano(), test.expectedResponse.Urn, "")
 			if err != nil {
 				t.Errorf("Test %v failed. Unexpected error counting users: %v", n, err)
 				continue
@@ -182,7 +182,7 @@ func TestPostgresRepo_GetUserByExternalID(t *testing.T) {
 				continue
 			}
 			// Check database
-			userNumber, err := getUsersCountFiltered("", test.externalID, "", 0, "")
+			userNumber, err := getUsersCountFiltered("", test.externalID, "", 0, "", "")
 			if err != nil {
 				t.Errorf("Test %v failed. Unexpected error counting users: %v", n, err)
 				continue
@@ -273,7 +273,7 @@ func TestPostgresRepo_GetUserByID(t *testing.T) {
 				continue
 			}
 			// Check database
-			userNumber, err := getUsersCountFiltered(test.userID, "", "", 0, "")
+			userNumber, err := getUsersCountFiltered(test.userID, "", "", 0, "", "")
 			if err != nil {
 				t.Errorf("Test %v failed. Unexpected error counting users: %v", n, err)
 				continue
@@ -299,7 +299,6 @@ func TestPostgresRepo_UpdateUser(t *testing.T) {
 		newUrn       string
 		// Expected result
 		expectedResponse *api.User
-		expectedError    *database.Error
 	}{
 		"OkCase": {
 			previousUser: &api.User{
@@ -339,38 +338,147 @@ func TestPostgresRepo_UpdateUser(t *testing.T) {
 		}
 		// Call to repository to store an user
 		updatedUser, err := repoDB.UpdateUser(*test.userToUpdate, test.newPath, test.newUrn)
-		if test.expectedError != nil {
-			dbError, ok := err.(*database.Error)
-			if !ok || dbError == nil {
-				t.Errorf("Test %v failed. Unexpected data retrieved from error: %v", n, err)
-				continue
-			}
-			if diff := pretty.Compare(dbError, test.expectedError); diff != "" {
-				t.Errorf("Test %v failed. Received different error response (received/wanted) %v", n, diff)
-				continue
-			}
-		} else {
-			if err != nil {
-				t.Errorf("Test %v failed. Unexpected error: %v", n, err)
-				continue
-			}
-			// Check response
-			if diff := pretty.Compare(updatedUser, test.expectedResponse); diff != "" {
-				t.Errorf("Test %v failed. Received different responses (received/wanted) %v", n, diff)
-				continue
-			}
-			// Check database
-			userNumber, err := getUsersCountFiltered(test.expectedResponse.ID, test.expectedResponse.ExternalID, test.expectedResponse.Path,
-				test.expectedResponse.CreateAt.UnixNano(), test.expectedResponse.Urn)
-			if err != nil {
-				t.Errorf("Test %v failed. Unexpected error counting users: %v", n, err)
-				continue
-			}
-			if userNumber != 1 {
-				t.Fatalf("Test %v failed. Received different user number: %v", n, userNumber)
-				continue
-			}
 
+		if err != nil {
+			t.Errorf("Test %v failed. Unexpected error: %v", n, err)
+			continue
+		}
+		// Check response
+		if diff := pretty.Compare(updatedUser, test.expectedResponse); diff != "" {
+			t.Errorf("Test %v failed. Received different responses (received/wanted) %v", n, diff)
+			continue
+		}
+		// Check database
+		userNumber, err := getUsersCountFiltered(test.expectedResponse.ID, test.expectedResponse.ExternalID, test.expectedResponse.Path,
+			test.expectedResponse.CreateAt.UnixNano(), test.expectedResponse.Urn, "")
+		if err != nil {
+			t.Errorf("Test %v failed. Unexpected error counting users: %v", n, err)
+			continue
+		}
+		if userNumber != 1 {
+			t.Fatalf("Test %v failed. Received different user number: %v", n, userNumber)
+			continue
+		}
+
+	}
+}
+
+func TestPostgresRepo_GetUsersFiltered(t *testing.T) {
+	now := time.Now().UTC()
+	testcases := map[string]struct {
+		// Previous data
+		previousUsers []api.User
+		// Postgres Repo Args
+		pathPrefix string
+		// Expected result
+		expectedResponse []api.User
+	}{
+		"OkCase1": {
+			previousUsers: []api.User{
+				api.User{
+					ID:         "UserID1",
+					ExternalID: "ExternalID1",
+					Path:       "Path123",
+					Urn:        "urn1",
+					CreateAt:   now,
+				},
+				api.User{
+					ID:         "UserID2",
+					ExternalID: "ExternalID2",
+					Path:       "Path456",
+					Urn:        "urn2",
+					CreateAt:   now,
+				},
+			},
+			pathPrefix: "Path",
+			expectedResponse: []api.User{
+				api.User{
+					ID:         "UserID1",
+					ExternalID: "ExternalID1",
+					Path:       "Path123",
+					Urn:        "urn1",
+					CreateAt:   now,
+				},
+				api.User{
+					ID:         "UserID2",
+					ExternalID: "ExternalID2",
+					Path:       "Path456",
+					Urn:        "urn2",
+					CreateAt:   now,
+				},
+			},
+		},
+		"OkCase2": {
+			previousUsers: []api.User{
+				api.User{
+					ID:         "UserID1",
+					ExternalID: "ExternalID1",
+					Path:       "Path123",
+					Urn:        "urn1",
+					CreateAt:   now,
+				},
+				api.User{
+					ID:         "UserID2",
+					ExternalID: "ExternalID2",
+					Path:       "Path456",
+					Urn:        "urn2",
+					CreateAt:   now,
+				},
+			},
+			pathPrefix: "Path123",
+			expectedResponse: []api.User{
+				api.User{
+					ID:         "UserID1",
+					ExternalID: "ExternalID1",
+					Path:       "Path123",
+					Urn:        "urn1",
+					CreateAt:   now,
+				},
+			},
+		},
+		"OkCase3": {
+			previousUsers: []api.User{
+				api.User{
+					ID:         "UserID1",
+					ExternalID: "ExternalID1",
+					Path:       "Path123",
+					Urn:        "urn1",
+					CreateAt:   now,
+				},
+				api.User{
+					ID:         "UserID2",
+					ExternalID: "ExternalID2",
+					Path:       "Path456",
+					Urn:        "urn2",
+					CreateAt:   now,
+				},
+			},
+			pathPrefix:       "NoPath",
+			expectedResponse: []api.User{},
+		},
+	}
+
+	for n, test := range testcases {
+		// Clean user database
+		cleanUserTable()
+
+		// Insert previous data
+		if test.previousUsers != nil {
+			for _, previousUser := range test.previousUsers {
+				insertUser(previousUser.ID, previousUser.ExternalID, previousUser.Path,
+					previousUser.CreateAt.UnixNano(), previousUser.Urn)
+			}
+		}
+		// Call to repository to get an user
+		receivedUsers, err := repoDB.GetUsersFiltered(test.pathPrefix)
+		if err != nil {
+			t.Errorf("Test %v failed. Unexpected error: %v", n, err)
+			continue
+		}
+		// Check response
+		if diff := pretty.Compare(receivedUsers, test.expectedResponse); diff != "" {
+			t.Errorf("Test %v failed. Received different responses (received/wanted) %v", n, diff)
+			continue
 		}
 
 	}
