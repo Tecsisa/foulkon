451295cd5187083ff37d34a9ca3f528d7f64f1e8
Gerson Pozo González <gerson.pozo.gonzalez@tecsisa.com>
2016-07-12 09:38:40 +0200

    Test GetGroupByName for postgres connector added

--- database/postgresql/group_test.go
+++ database/postgresql/group_test.go
@@ -1,6 +1,7 @@
 package postgresql
 
 import (
+	"fmt"
 	"github.com/kylelemons/godebug/pretty"
 	"github.com/tecsisa/authorizr/api"
 	"github.com/tecsisa/authorizr/database"
@@ -67,8 +68,12 @@ func TestPostgresRepo_AddGroup(t *testing.T) {
 
 		// Insert previous data
 		if test.previousGroup != nil {
-			insertGroup(test.previousGroup.ID, test.previousGroup.Name, test.previousGroup.Path,
+			err := insertGroup(test.previousGroup.ID, test.previousGroup.Name, test.previousGroup.Path,
 				test.previousGroup.CreateAt.UnixNano(), test.previousGroup.Urn, test.previousGroup.Org)
+			if err != nil {
+				t.Errorf("Test %v failed. Unexpected error inserting previous data: %v", n, err)
+				continue
+			}
 		}
 		// Call to repository to store group
 		storedGroup, err := repoDB.AddGroup(*test.groupToCreate)
@@ -104,6 +109,93 @@ func TestPostgresRepo_AddGroup(t *testing.T) {
 				continue
 			}
 
+		}
+
+	}
+}
+
+func TestPostgresRepo_GetGroupByName(t *testing.T) {
+	now := time.Now().UTC()
+	testcases := map[string]struct {
+		// Previous data
+		previousGroup *api.Group
+		// Postgres Repo Args
+		org  string
+		name string
+		// Expected result
+		expectedResponse *api.Group
+		expectedError    *database.Error
+	}{
+		"OkCase": {
+			previousGroup: &api.Group{
+				ID:       "GroupID",
+				Name:     "Name",
+				Path:     "Path",
+				Urn:      "Urn",
+				CreateAt: now,
+				Org:      "Org",
+			},
+			org:  "Org",
+			name: "Name",
+			expectedResponse: &api.Group{
+				ID:       "GroupID",
+				Name:     "Name",
+				Path:     "Path",
+				Urn:      "Urn",
+				CreateAt: now,
+				Org:      "Org",
+			},
+		},
+		"ErrorCaseGroupNotExist": {
+			previousGroup: &api.Group{
+				ID:       "GroupID",
+				Name:     "Name",
+				Path:     "Path",
+				Urn:      "Urn",
+				CreateAt: now,
+				Org:      "Org",
+			},
+			org:  "Org",
+			name: "NotExist",
+			expectedError: &database.Error{
+				Code:    database.GROUP_NOT_FOUND,
+				Message: fmt.Sprintf("Group with organization Org and name NotExist not found"),
+			},
+		},
+	}
+
+	for n, test := range testcases {
+		// Clean group database
+		cleanGroupTable()
+
+		// Insert previous data
+		if test.previousGroup != nil {
+			err := insertGroup(test.previousGroup.ID, test.previousGroup.Name, test.previousGroup.Path,
+				test.previousGroup.CreateAt.UnixNano(), test.previousGroup.Urn, test.previousGroup.Org)
+			if err != nil {
+				t.Errorf("Test %v failed. Unexpected error inserting previous data: %v", n, err)
+				continue
+			}
+		}
+
+		// Call to repository to get group
+		receivedGroup, err := repoDB.GetGroupByName(test.org, test.name)
+		if test.expectedError != nil {
+			dbError, ok := err.(*database.Error)
+			if !ok || dbError == nil {
+				t.Errorf("Test %v failed. Unexpected data retrieved from error: %v", n, err)
+				continue
+			}
+		} else {
+			if err != nil {
+				t.Errorf("Test %v failed. Unexpected error: %v", n, err)
+				continue
+			}
+			// Check response
+			if diff := pretty.Compare(receivedGroup, test.expectedResponse); diff != "" {
+				t.Errorf("Test %v failed. Received different responses (received/wanted) %v", n, diff)
+				continue
+			}
 		}
 
 	}
