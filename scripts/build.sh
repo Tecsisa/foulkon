6b37af52176b3786b17e92ad2197cc492431d671
Adrián López Gómez <adrian.lopez.gomez@tecsisa.com>
2016-09-01 15:32:37 +0200

    Push foulkon docker images to Dockerhub using TravisCI

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 echo "--> Building..."
 #Make sure $GOPATH is set
-CGO_ENABLED=0 go install github.com/tecsisa/foulkon/cmd/worker
-CGO_ENABLED=0 go install github.com/tecsisa/foulkon/cmd/proxy
+CGO_ENABLED=0 go install github.com/Tecsisa/foulkon/cmd/worker
+CGO_ENABLED=0 go install github.com/Tecsisa/foulkon/cmd/proxy
 
 mkdir bin/ 2>/dev/null
 cp $GOPATH/bin/worker ./bin
 cp $GOPATH/bin/proxy ./bin
 
-echo "==> Building Docker images..."
-docker build -t tecsisa/foulkon-proxy -f scripts/docker/Dockerfile_proxy .
-docker build -t tecsisa/foulkon-worker -f scripts/docker/Dockerfile_worker .
-echo "Docker images built!"
-docker images | grep "tecsisa/foulkon"
+echo "----> Building Docker images..."
+docker build -t tecsisa/foulkon-proxy:latest -f scripts/docker/Dockerfile_proxy .
+docker build -t tecsisa/foulkon-worker:latest -f scripts/docker/Dockerfile_worker .
+echo "----> Pushing images to Docker hub"
+curl -u ${DOCKER_HUB_USER}:${DOCKER_HUB_KEY} https://cloud.docker.com/api/app/v1/service/
+docker push tecsisa/foulkon-worker:latest
+docker push tecsisa/foulkon-proxy:latest
