ee29307da77c949b868d3fbcd65cfb24b3ddf034
Adrián López Gómez <adrian.lopez.gomez@tecsisa.com>
2016-09-01 16:08:37 +0200

    force Go version in travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,9 +9,8 @@ cp $GOPATH/bin/worker ./bin
 cp $GOPATH/bin/proxy ./bin
 
 echo "----> Building Docker images..."
-docker build -t tecsisa/foulkon-proxy:latest -f scripts/docker/Dockerfile_proxy .
-docker build -t tecsisa/foulkon-worker:latest -f scripts/docker/Dockerfile_worker .
+docker build -t tecsisa/foulkon:latest -f scripts/docker/Dockerfile .
+
 echo "----> Pushing images to Docker hub"
-curl -u ${DOCKER_HUB_USER}:${DOCKER_HUB_KEY} https://cloud.docker.com/api/app/v1/service/
-docker push tecsisa/foulkon-worker:latest
-docker push tecsisa/foulkon-proxy:latest
+docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_KEY}
+docker push tecsisa/foulkon:latest
